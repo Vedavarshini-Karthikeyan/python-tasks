34d4c89eacbdbae2778a8ec57a1db52d1807f63c
Vedavarshini <vedavarshinikarthikeyan@gmail.com>
2026-09-04 17:27:59 +0530

    commiting the function & module concept

---
+++ function_concept.py
@@ -0,0 +1,39 @@
+#basic function concept
+num1 = 7
+num2 = 2
+
+def add():
+    add = 7+2
+    print("Addition of two numbers: ", str(add))
+
+def sub():
+    sub = 7-2
+    print("Subtraction of two numbers: ", str(sub))
+
+def mul():
+    mul = 7*2
+    print("Multiplication of two numbers: ", str(mul))
+
+#calling the function
+add()
+sub()
+mul()
+
+
+
+#Concept of taking the input and returing the oupput
+def addition(number1, number2):
+    addition = number1 + number2
+    return addition
+
+def subtraction(number1, number2):
+    subtraction = number1 + number2
+    return subtraction
+
+def multiplication(number1, number2):
+    multiplication = number1 + number2
+    return multiplication
+
+print(addition(28,7))
+print(subtraction(28,7))
+print(multiplication(28,7))
---
+++ module_concept.py
@@ -0,0 +1,6 @@
+import function_concept as use_function
+
+#calls the enitre function in the function_concept
+use_function
+
+
