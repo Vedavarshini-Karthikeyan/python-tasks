7eba8f66fa37dddd3746568588e4e5070b4017dd
Vedavarshini <vedavarshinikarthikeyan@gmail.com>
2026-09-06 16:07:16 +0530

    practicing the operators in python

---
+++ operators.py
@@ -0,0 +1,37 @@
+#Arithmetic Operator
+num1 = 8
+num2 = 2
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+print("Addition of two numbers: ", add)
+print("Difference of two numbers: ", sub)
+print("Product of two numbers: ", mul)
+print("Division of two numbers: ", div)
+print("Modulus of two numbers: ", mod)
+
+#Comparison Operator
+print(num1<num2)
+print(num1>num2)
+print(num1<=num2)
+print(num1>=num2)
+print(num1==num2)
+print(num1!=num2)
+
+#Logical Operators
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+
+#Assigment Operator
+total = 10
+total += 2
+total -= 2
+total *= 2
+total /= 2
+print(total)
